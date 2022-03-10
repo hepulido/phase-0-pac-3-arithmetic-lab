ff563bbf63aa8bcf7abf4455720ca23243526a39
hector <hpulido1021@gmail.com>
2022-03-09 22:39:58 -0800

    Completed assingment

---
+++ index.js
@@ -0,0 +1,36 @@
+let sum;
+let number;
+
+function add (num1, num2){
+ return sum = num1 +num2;
+}
+
+function subtract (num1, num2){
+    return sum = num1 - num2;
+}
+
+function multiply (num1, num2){
+    return sum = num1 * num2;
+}
+
+function divide (num1, num2){
+    return sum = num1 / num2;
+}
+
+function increment(n){
+    return  ++n;
+}
+
+function decrement (n){
+    return  --n;
+}
+
+function makeInt (n){
+    return parseInt(n,10);
+}
+
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
+
